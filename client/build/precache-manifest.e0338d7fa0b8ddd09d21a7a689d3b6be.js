57719ae5bc9fbf0bfaed1767c660473922b9e32d
Evgeniy <ekovalwork@gmail.com>
2019-12-26 14:30:55 +0200

    Build

---
+++ client/build/precache-manifest.e0338d7fa0b8ddd09d21a7a689d3b6be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b4c3e3f35fad5392a7212bd42f97a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746c5cdb3a6d5d6fba7f",
+    "url": "/static/css/main.fe4b4b48.chunk.css"
+  },
+  {
+    "revision": "3bde377cd2980ac7cac8",
+    "url": "/static/js/2.05b3dfa6.chunk.js"
+  },
+  {
+    "revision": "edd9f9f3fccf0b7686cff7d83e1a4e2e",
+    "url": "/static/js/2.05b3dfa6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "746c5cdb3a6d5d6fba7f",
+    "url": "/static/js/main.89b3ce28.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "faecf6290063947242064b134fc10210",
+    "url": "/static/media/bg.faecf629.jpg"
+  }
+]);
